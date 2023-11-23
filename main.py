1212b6f571090627948e316ae9203405ebd3f842
Rory-luo <55289804+Rory-luo@users.noreply.github.com>
2023-11-23 16:18:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import requests
 import os
 import random
 import json
+from bs4 import BeautifulSoup
 
 nowtime = datetime.utcnow() + timedelta(hours=8)  # 东八区时间
 today = datetime.strptime(str(nowtime.date()), "%Y-%m-%d")  # 今天的日期
@@ -17,7 +18,6 @@ city_location_dict = city_location_dict.replace("'", "\"")  # 将单引号替换
 print(city_location_dict)
 print(type(city_location_dict))
 city_location_dict = json.loads(city_location_dict)
-uv_key = os.getenv('UV_KEY')
 birthday = os.getenv('BIRTHDAY')
 period = os.getenv('PERIOD')
 
@@ -25,8 +25,7 @@ app_id = os.getenv('APP_ID')
 app_secret = os.getenv('APP_SECRET')
 
 user_ids = os.getenv('USER_ID', '').split("\n")
-template_id = os.getenv('TEMPLATE_ID')
-after_template_id = os.getenv('AFTER_TEMPLATE_ID')
+night_template_id = os.getenv('NIGHT_TEMPLATE_ID')
 end_words = os.getenv('END_WORDS')
 
 
@@ -38,12 +37,8 @@ if not user_ids:
     print('请设置 USER_ID，若存在多个 ID 用回车分开')
     exit(422)
 
-if template_id is None:
-    print('请设置 TEMPLATE_ID')
-    exit(422)
-
-if after_template_id is None:
-    print('请设置 AFTER_TEMPLATE_ID')
+if night_template_id is None:
+    print('请设置 NIGHT_TEMPLATE_ID')
     exit(422)
 
 
@@ -82,8 +77,9 @@ def get_weather():
             airQuality = evaluate_air_quality()
             airData = get_air_quality()
             wind = data['wind']['deg']
-            uv_value = evaluate_uv_level(get_uv_index(), temperature)
-            return {'weather': weather, 'temperature': temperature, 'humidity': humidity, 'high': highest, 'low': lowest, 'airQuality': airQuality, 'airData': airData, 'wind': wind, 'uv': uv_value}
+            # uv_value = evaluate_uv_level(get_uv_index(), temperature)
+            # return {'weather': weather, 'temperature': temperature, 'humidity': humidity, 'high': highest, 'low': lowest, 'airQuality': airQuality, 'airData': airData, 'wind': wind, 'uv': uv_value}
+            return {'weather': weather, 'temperature': temperature, 'humidity': humidity, 'high': highest, 'low': lowest, 'airQuality': airQuality, 'airData': airData, 'wind': wind}
         elif response.status_code == 401:
             print("API key is invalid or unauthorized. Please check your API key.")
             return exit(502)
@@ -94,49 +90,49 @@ def get_weather():
         print(f"An error occurred while making the request: {e}")
         return exit(502)
 
-# 获取当前地区的紫外线强度
-def get_uv_index():
-    base_url = "https://api.openuv.io/api/v1/uv"            # The url is used to get the UV value
-    headers = {
-        "x-access-token": uv_key                            # The key is from the openuv.io
-    }
-    params = {
-        "lat": city_location_dict[city]['lat'],
-        "lng": city_location_dict[city]['lon']
-    }
+# # 获取当前地区的紫外线强度
+# def get_uv_index():
+#     base_url = "https://api.openuv.io/api/v1/uv"            # The url is used to get the UV value
+#     headers = {
+#         "x-access-token": uv_key                            # The key is from the openuv.io
+#     }
+#     params = {
+#         "lat": city_location_dict[city]['lat'],
+#         "lng": city_location_dict[city]['lon']
+#     }
 
-    try:
-        response = requests.get(base_url, headers=headers, params=params)
-        data = response.json()
+#     try:
+#         response = requests.get(base_url, headers=headers, params=params)
+#         data = response.json()
 
-        if response.status_code == 200:
-            uv_index = data["result"]["uv"]
-            return uv_index
-        else:
-            print(f"无法获取紫外线强度信息。")
-            exit(422)
-    except Exception as e:
-        print(f"发生错误: {str(e)}")
-        exit(422)
+#         if response.status_code == 200:
+#             uv_index = data["result"]["uv"]
+#             return uv_index
+#         else:
+#             print(f"无法获取紫外线强度信息。")
+#             exit(422)
+#     except Exception as e:
+#         print(f"发生错误: {str(e)}")
+#         exit(422)
 
 
-# 判断紫外线的强度等级
-def evaluate_uv_level(uv_value, temperature_value):
-    if uv_value is None or temperature_value is None:
-        exit(422)
-    else:
-        if 0 <= uv_value < 3 and temperature_value >= 15:
-            return '1级，偏低，但是气温稍高，注意长时间外出的防晒工作哦~'
-        elif 0 <= uv_value < 3 and temperature_value < 15:
-            return '1级，偏低，气温也稍微低，适合外出'
-        elif 3 <= uv_value < 6:
-            return '2级，中等，注意减少阳光直晒*'
-        elif 6 <= uv_value < 8:
-            return '3级，高, 30~60秒便可晒红皮肤。外出请采取防护措施'
-        elif 8 <= uv_value < 11:
-            return '4级，甚高，只要30秒左右时间便可晒红皮肤，中午前后宜减少外出'
-        else:
-            return '5级，极高，不到20秒便可晒红皮肤，一般人都应避免外出，或采取特殊的防护'
+# # 判断紫外线的强度等级
+# def evaluate_uv_level(uv_value, temperature_value):
+#     if uv_value is None or temperature_value is None:
+#         exit(422)
+#     else:
+#         if 0 <= uv_value < 3 and temperature_value >= 15:
+#             return '1级，偏低，但是气温稍高，注意长时间外出的防晒工作哦~'
+#         elif 0 <= uv_value < 3 and temperature_value < 15:
+#             return '1级，偏低，气温也稍微低，适合外出'
+#         elif 3 <= uv_value < 6:
+#             return '2级，中等，注意减少阳光直晒*'
+#         elif 6 <= uv_value < 8:
+#             return '3级，高, 30~60秒便可晒红皮肤。外出请采取防护措施'
+#         elif 8 <= uv_value < 11:
+#             return '4级，甚高，只要30秒左右时间便可晒红皮肤，中午前后宜减少外出'
+#         else:
+#             return '5级，极高，不到20秒便可晒红皮肤，一般人都应避免外出，或采取特殊的防护'
 
             
 # 获取当前地区的空气质量数值
@@ -219,44 +215,49 @@ def get_period_left():
         next_month_period = datetime.strptime(str(today.year) + "-" + str(today.month + 1) + "-" + period, "%Y-%m-%d")
     interval_period = (nowtime - this_month_period).days
     words_list = ['辛苦我的瑶瑶了', '真棒，最后一天了', '抱抱瑶瑶~', '委屈瑶瑶啦']
-    # words_list = ['辛苦我的小馋猫了', '真棒，最后一天了', '抱抱我的小美女', '委屈我的小美女啦']
     if 0 <= interval_period <= 6:
         if interval_period == 6:
             words_reply = "瑶瑶例假来了第{0}天，{1}".format(interval_period + 1, words_list[random.randint(0, 3)])
-            # words_reply = "我家小美女大姨妈来了第{0}天，{1}".format(interval_period + 1, words_list[random.randint(0, 3)])
             return words_reply
         words_reply = "今天是瑶瑶例假来的第{0}天".format(interval_period + 1)
-        # words_reply = "今天是小馋猫例假来的第{0}天".format(interval_period + 1)
         return words_reply
     elif interval_period < 0:
         if 0 <= (nowtime - last_month_period).days <= 6:
             if (nowtime - last_month_period).days == 6:
                 words_reply = "瑶瑶大姨妈来了第{0}天，{1}".format(interval_period + 1, words_list[random.randint(0, 3)])
-                # words_reply = "我家小美女大姨妈来了第{0}天，{1}".format(interval_period + 1, words_list[random.randint(0, 3)])
                 return words_reply
             words_reply = "今天是瑶瑶例假来的第{0}天".format(interval_period + 1)
-            # words_reply = "今天是小馋猫例假来的第{0}天".format(interval_period + 1)
             return words_reply
         words_reply = "报告，瑶瑶的例假预计还有{0}天到".format((this_month_period - nowtime).days + 1)
-        # words_reply = "报告，小馋猫的大姨妈预计还有{0}天到".format((this_month_period - nowtime).days + 1)
         return words_reply
     else:
         words_reply = "报告瑶瑶，大姨妈预计还有{0}天到".format((next_month_period - nowtime).days + 1)
-        # words_reply = "报告小馋猫，我家小美女的大姨妈预计还有{0}天到".format((next_month_period - nowtime).days + 1)
         return words_reply
-# def get_period_left():
-#     if period is None:
-#         print('没有设置 PERIOD')
-#         return 0
-#     next_time = datetime.strptime(str(today.year) + "-" + str(today.month) + "-" + period, "%Y-%m-%d")
-#     next_time_period = (datetime.strptime(str(today.year) + "-" + str(today.month) + "-" + period, "%Y-%m-%d") + timedelta(days=7))
-#     if next_time < nowtime < next_time_period:
-#         words_reply = "今天是小馋猫例假来的第{0}天".format((today - next_time).days + 1)
-#         return words_reply
-#     if next_time.day > nowtime.day:
-#         # next_time = next_time.replace(month=next_time.month + 1)
-#         words_reply = "距离小馋猫的例假来临还有{0}天".format((next_time - today).days)
-#         return words_reply
+
+
+def get_holiday():
+    url = "https://www.daojishi321.com"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+    
+        # Find the countdown element
+        countdown_element = soup.find('div', class_='J_countdown')
+    
+        if countdown_element:
+            # Extract holiday name and countdown days
+            holiday_name = countdown_element.find('strong').text.strip()
+            countdown_days = countdown_element.find('span', class_='time day').text.strip()
+    
+            result = f"距离 {holiday_name} 还有{countdown_days}天"
+            return result
+        else:
+            print("Countdown element not found on the page.")
+            return exit(522)
+    else:
+        print("Failed to retrieve the webpage. Status code:", response.status_code)
+        return exit(522)
 
 
 def get_end_words():
@@ -352,10 +353,10 @@ data = {
         "value": weather['wind'],
         "color": get_random_color()
     },
-    "uv": {
-        "value": weather['uv'],
-        "color": get_random_color()
-    },
+    # "uv": {
+    #     "value": weather['uv'],
+    #     "color": get_random_color()
+    # },
     "love_days": {
         "value": get_memorial_days_count(),
         "color": get_random_color()
@@ -376,21 +377,18 @@ data = {
         "value": get_end_words(),
         "color": get_random_color()
     },
+    "holiday": {
+        "value": get_holiday(),
+        "color": get_random_color()
+    },
 }
 
 if __name__ == '__main__':
     count = 0
     try:
         for user_id in user_ids:
-            if 0 <= int(nowtime.hour) < 12:
-                res = wm.send_template(user_id, template_id, data)
-                count += 1
-            elif 12 < int(nowtime.hour) <= 18:
-                res = wm.send_template(user_id, after_template_id, data)
-                count += 1
-            else:
-                res = wm.send_template(user_id, after_template_id, data)
-                count += 1
+            res = wm.send_template(user_id, nignt_template_id, data)
+            count += 1
     except WeChatClientException as e:
         print('微信端返回错误：%s。错误代码：%d' % (e.errmsg, e.errcode))
         exit(502)
